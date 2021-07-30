f45f4a4e4d6bcbc6408c1a92f304cbaacf712739
Gabriel Campos Lopes <gcamposlopes@gmail.com>
2021-07-30 00:19:50 -0300

    first end point

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Aplicação rodando na porta 5000");
+app.get("/", (request, response) => {
+    console.log("Hello World! First END POINT ;D");
+    response.json({
+        message: "Hello World! First END POINT ;D"
+    });//enviar
 });
 
 
+app.listen(5000, () => {
+    console.log("Aplicação rodando na porta 5000");
+});
